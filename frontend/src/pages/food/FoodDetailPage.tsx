0bb7810bc21f504e2793b2d30a4080180691fc68
viet <vietunitydev2222@gmail.com>
2025-05-12 02:34:59 +0700

    - remove return

--- frontend/src/pages/food/FoodDetailPage.tsx
+++ frontend/src/pages/food/FoodDetailPage.tsx
@@ -98,7 +98,7 @@ const FoodDetailPage: React.FC = () => {
                     <p className="rating">Đánh giá: {foodItem.rating} / 5</p>
                     {foodItem.isFeature && <p className="feature">Món ăn nổi bật</p>}
                     <div className="buttons">
-                        <Link to="/food" className="back-btn">Quay về</Link>
+                        {/*<Link to="/food" className="back-btn">Quay về</Link>*/}
                         <button onClick={addToCart} className="add-to-cart-btn">
                             Thêm vào giỏ hàng
                         </button>
